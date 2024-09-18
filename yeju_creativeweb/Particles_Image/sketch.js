5fed85474a754618aadf8b8bfc1f624b73b1bc9a
bomimongmong <yejukang98@gmail.com>
2024-09-18 02:25:39 -0500

    Particle (5)

--- yeju_creativeweb/Particles_Image/sketch.js
+++ yeju_creativeweb/Particles_Image/sketch.js
@@ -8,15 +8,8 @@ let imgUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Claude_M
 let img;
 let particles = [];
 
-// Sound variables
-let sound;
-let soundPlaying = false;
-let lastMouseMoveTime = 0;
-const SOUND_PLAY_DURATION = 1000;  // Duration to keep the sound playing after the last mouse move
-
 function preload() {
     img = loadImage(imgUrl);
-    sound = loadSound('Particles_Image/Hit2.mp3');  // Replace with your sound file URL
 }
 
 function setup() {
@@ -26,7 +19,6 @@ function setup() {
 
 function draw() {
     background(40);
-    //image(img, 0, 0, width, height);  // Ensure the image is redrawn
 
     particles.forEach((particle, index) => {
         particle.update();
@@ -37,45 +29,42 @@ function draw() {
             particles.splice(index, 1);
         }
     });
-
-    // Manage sound playback
-    if (millis() - lastMouseMoveTime < SOUND_PLAY_DURATION) {
-        if (!soundPlaying) {
-            sound.loop();  // Start playing the sound in a loop
-            soundPlaying = true;
-        }
-    } else {
-        if (soundPlaying) {
-            sound.stop();  // Stop playing the sound
-            soundPlaying = false;
-        }
-    }
-}
-
-// This function is called whenever the mouse is moved
-function mouseMoved() {
-    lastMouseMoveTime = millis();  // Update the time of the last mouse movement
 }
 
 function spawnParticles() {
     for (let i = 0; i < width; i += RESOLUTION) {
         for (let j = 0; j < height; j += RESOLUTION) {
+            
             let x = map(i, 0, width, 0, img.width);
             let y = map(j, 0, height, 0, img.height);
+
+            
             const color = img.get(x, y);
-            particles.push(new Particle(i + particleSize / 2, j + particleSize / 2, color));
+
+            particles.push(
+                new Particle(i + particleSize / 2, j + particleSize / 2, color)
+            );
         }
     }
 }
 
 class Particle {
     constructor(x, y, color) {
+        // Initial position
         this.pos = createVector(x, y);
         this.target = createVector(x, y);
+
+        // Initial velocity and acceleration set to zero
         this.vel = createVector(0, 0);
         this.acc = createVector(0, 0);
+
+        // Color of the particle (preserved from the image)
         this.color = color;
+
+        // Age of the particle (starts from the maximum)
         this.age = MAX_AGE;
+
+        // Size factor based on age
         this.size = particleSize;
     }
 
@@ -85,36 +74,56 @@ class Particle {
 
     update() {
         let mouseVector = createVector(mouseX, mouseY);
+    
+        // Calculate vectors to determine forces
         let fromMouseToParticle = p5.Vector.sub(this.pos, mouseVector);
         let distanceToMouse = fromMouseToParticle.mag();
         let fromParticleToTarget = p5.Vector.sub(this.target, this.pos);
         let distanceToTarget = fromParticleToTarget.mag();
-
+    
         let totalForce = createVector(0, 0);
-        let randomBrushSize = random(0, 70);
-
+    
+        // Generate a random brush size for the mouse effect (between 0 and 70)
+        let randomBrushSize = random(0, 150);
+    
+        // Repulsive force from the mouse if within the random brush size
         if (distanceToMouse < randomBrushSize) {
             let repulsionForce = map(distanceToMouse, 0, randomBrushSize, MAX_FORCE, MIN_FORCE);
             fromMouseToParticle.setMag(repulsionForce);
             totalForce.add(fromMouseToParticle);
         }
-
+    
+        // Attractive force towards the particle's target
         let attractionForce = map(distanceToTarget, 0, 100, MIN_FORCE, MAX_FORCE);
         fromParticleToTarget.setMag(attractionForce);
         totalForce.add(fromParticleToTarget);
-
+    
+        // Apply total force to acceleration
         this.applyForce(totalForce);
+    
+        // Update velocity based on acceleration
         this.vel.add(this.acc);
+    
+        // Update position based on velocity
         this.pos.add(this.vel);
+    
+        // Dampen velocity slightly (for smoother stopping)
         this.vel.mult(0.95);
+    
+        // Reset acceleration for the next frame
         this.acc.mult(0);
-
+    
+        // Age the particle and decrease its size as it gets older
         this.age -= 0.25;
-        let sizeFactor = map(this.age, MAX_AGE, 0, 1.5, 0);
+    
+        // Size grows and then shrinks based on age
+        let sizeFactor = map(this.age, MAX_AGE, 0, 1.5, 0);  
         this.size = particleSize * sizeFactor;
     }
+    
 
     draw() {
+        
         fill(this.color);
         noStroke();
         ellipse(this.pos.x, this.pos.y, this.size);
